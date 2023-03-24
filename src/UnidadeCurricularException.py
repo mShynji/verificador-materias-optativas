809e6c3806378997a5072cc4ee2e1d6ec7fed166
mira-oza <m.shynjirobbie@gmail.com>
2023-03-24 11:18:02 -0400

    fix: super()

--- src/UnidadeCurricularException.py
+++ src/UnidadeCurricularException.py
@@ -9,4 +9,4 @@ class UnidadeCurricularException(Exception):
 	def __init__(self, valor:str, message:str):
 		self.valor = valor
 		self.message = message
-		super.__init__(self.message)
+		super().__init__(self.message)

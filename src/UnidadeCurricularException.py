ae9dbdd5d3e21cc3475c6284af06bc69bd54e6b0
mshynji <m.shynjirobbie@gmail.com>
2023-07-10 14:56:48 -0400

    fix: trocado def para class

--- src/UnidadeCurricularException.py
+++ src/UnidadeCurricularException.py
@@ -1,5 +1,5 @@
-def UCException(Exception):
-    def __init__(self, valor: str, mensagem: str):
+class UCException(Exception):
+    def __init__(self, valor: str, mensagem: str) -> None:
         self.valor    = valor
         self.mensagem = mensagem
         super().__init__(self.mensagem)
